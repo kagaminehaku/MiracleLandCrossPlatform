38de628421c7c74abbea77aa3317375ba2446768
Kagamine Haku <blomaway@gmail.com>
2024-02-28 13:58:16 +0700

    Add project files.

---
+++ MiracleLandCrossPlatform/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MiracleLandCrossPlatform
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MiracleLandCrossPlatform/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MiracleLandCrossPlatform
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewPage1());
+        }
+    }
+
+}
