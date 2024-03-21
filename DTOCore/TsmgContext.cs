0e39d08b2a95e546ce049986ffdd567498bb8c05
KagamineHaku <blomaway@gmail.com>
2024-03-21 11:51:52 +0700

    Bug fix

--- DTOCore/TsmgContext.cs
+++ DTOCore/TsmgContext.cs
@@ -27,8 +27,8 @@ public partial class TsmgContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=TSMG;user id=sa;password=17102003;Trusted_Connection=True;TrustServerCertificate=True;");
-
+       //=> optionsBuilder.UseSqlServer("Server=.;Database=TSMG;user id=sa;password=17102003;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=34.147.198.164;Database=TSMG;user id=sas;password=17102003;TrustServerCertificate=True;");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Order>(entity =>
